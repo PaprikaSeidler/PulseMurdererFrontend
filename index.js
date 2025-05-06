d5e7b76ff7746ddb351152aedb47458b60f80baf
Miki-Aqqalu Kruse <mstachkruse@gmail.com>
2025-05-06 11:08:48 +0200

    Changed Players.player to Players.name

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 Vue.createApp({
     data() {
-      return {
-          Id : 0,
-          Player: "",
-          Muderer: true,
-          Winner: false,
-          player1Id: null,
-          player2Id: null,
-          voteCount: 0,
-        
-          Players: [
-              { Id: 1, Player: "Peter", role: 'Murderer' },
-              { Id: 2, Player: "John", role: 'Player' },
-              { Id: 3, Player: "Mary", role: 'Player' },
-              { Id: 4, Player: "Sophie", role: 'Player' },
-              { Id: 5, Player: "Tom", role: 'Player' },
-          ],
-          result: ''
-      };
+        return {
+            id : 0,
+            name: "",
+            Muderer: true,
+            Winner: false,
+            player1Id: null,
+            player2Id: null,
+            voteCount: 0,
+            voterButtonClicked: false,
+
+            Players: [
+                { id: 1, name: "Peter", role: 'Murderer' },
+                { id: 2, name: "John", role: 'name' },
+                { id: 3, name: "Mary", role: 'name' },
+                { id: 4, name: "Sophie", role: 'name' },
+                { id: 5, name: "Tom", role: 'name' },
+            ],
+            result: ''
+        };
     },
     methods: {
-      determineWinner() {
-          const player1 = this.Players.find(player => player.Id === Number(this.player1Id));
-          const player2 = this.Players.find(player => player.Id === Number(this.player2Id));
+        determineWinner() {
+            const player1 = this.Players.find(player => player.id === Number(this.player1Id));
+            const player2 = this.Players.find(player => player.id === Number(this.player2Id));
 
-          if (!player1 || !player2) {
-              this.result = 'Invalid player IDs. Please try again.';
-              return;
-          }
+            if (!player1 || !player2) {
+                this.result = 'Invalid player IDs. Please try again.';
+                return;
+            }
 
-          if (player1.role === 'Murderer' || player2.role === 'Murderer') {
-              this.result = 'The Murderer wins!';
-          } else {
-              this.result = 'The two players win!';
-          }
-      },
-      vote() {
-          
-      },
+            if (player1.role === 'Murderer' || player2.role === 'Murderer') {
+                this.result = 'The Murderer wins!';
+            } else {
+                this.result = 'The two players win!';
+            }
+        },
+        vote(id) {
+            if(voterButtonClicked === false){
+                voterButtonClicked = true
+            }
+            else if(voterButtonClicked === true){
+                voterButtonClicked = false
+            }
+        },
     }
 }).mount('#app');
